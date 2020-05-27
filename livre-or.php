96f63eeda7564c02b515adba2fb0c20c8066f4c3
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2020-05-27 15:19:37 +0200

    creation de la page livre-or

---
+++ livre-or.php
@@ -0,0 +1,55 @@
+<?php 
+$title = "Livre d'or";
+require 'php/require/header.php';
+
+$bd = mysqli_connect("localhost","root","","livreor");
+
+$commentaire_total = "SELECT COUNT(id) FROM commentaires ";
+$query_commentaire_total = mysqli_query($bd,$commentaire_total);
+$total = mysqli_fetch_all($query_commentaire_total);
+
+
+if (isset($_GET['page']) && !empty($_GET['page']) && $_GET['page'] > 0)
+{
+    $_GET['page'] = intval($_GET['page']);
+    $page_courante = $_GET['page'];
+}
+else {
+    $page_courante = 1;
+}
+
+$commantaire_par_page = 3;
+$depart = ( $page_courante - 1)*$commantaire_par_page; 
+$pageTotal = ceil($total[0][0]/$commantaire_par_page);
+
+
+$requete = "SELECT * , DATE_FORMAT(date, '%d/%m/%Y') AS date FROM `commentaires` inner join utilisateurs ON utilisateurs.id= id_utilisateur  ORDER BY date DESC LIMIT $depart,$commantaire_par_page"; // inner join pour aller chercher le nom de l utilisateur
+$query = mysqli_query($bd,$requete);
+$resultat = mysqli_fetch_all($query,MYSQLI_ASSOC);
+
+
+
+
+?>
+
+    <h2 class="titre">Livre d'or</h2>
+
+<?php foreach ($resultat as $value) : ?>
+    <section class="livreor_section">
+        <h3>Posté le <?php echo $value['date'] ?> , par <span><?php echo $value['login'] ?></span> </h3>
+        <p><?php echo $value['commentaire'] ?></p>
+    </section>
+<?php endforeach ;?>
+
+<nav class="nav-pagination">
+    <?php for ($i = 1 ; $i <= $pageTotal ; $i++) : ?>
+        <a href='livre-or.php?page=<?php echo $i?>'><?php echo $i ?></a> 
+    <?php endfor ; ?>
+</nav>
+
+<?php if (!empty($_SESSION['connect']) == 1) : ?>
+    <a class="connexion" href="commentaire.php">Ajouter un commenatire</a>
+<?php endif ;?>
+
+<?php require 'php/require/footer.html' ;?>
+
